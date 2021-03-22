2000f733b0033ca06ebf2d27c23c16a72d75babb
TheShah1001 <76670655+TheShah1001@users.noreply.github.com>
2021-03-22 15:46:50 -0400

    Add files via upload

---
+++ ball.js
@@ -0,0 +1,28 @@
+class Ball{
+    constructor(x,y,r)
+    {
+        var options={
+            isStatic:false,
+            restritution:0.3,
+            friction:0.5,
+            density:1.1
+
+        }
+        this.x=x;
+        this.y=y;
+        this.r=r;
+        this.body=Bodies.circle(x,y,r,options);
+        World.add(world, this.body);
+    }
+    display()
+    {
+        var ballpos=this.body.position;
+        
+        push()
+        fill("blue")
+        translate(ballpos.x,ballpos.y);
+        ellipseMode(RADIUS)
+        ellipse(0,0,this.r,this.r)
+        pop()
+    }
+}
